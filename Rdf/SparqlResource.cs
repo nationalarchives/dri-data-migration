4dde79f2285eab816157919b3340c784ab7bb767
Wojciech Stawiarski <Wojciech.Stawiarski@nationalarchives.gov.uk>
2025-07-18 14:59:04 +0100

    Missing dependencies

---
+++ Rdf/SparqlResource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using VDS.RDF;
+
+namespace Rdf;
+
+static class SparqlResource
+{
+    private static readonly Uri idNamespace = new("http://id.example.com/");
+    
+    internal static string GetEmbeddedSparql(Assembly assembly, string baseName, string fileName)
+    {
+        using var stream = assembly.GetManifestResourceStream($"{baseName}.{fileName}.sparql");
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+
+    internal static IUriNode NewId => new UriNode(new Uri(idNamespace, Guid.NewGuid().ToString()));
+
+}
