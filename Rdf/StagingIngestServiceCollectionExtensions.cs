7d561be8f1b20c2bd1af87e4eb41d5e6e74d8213
Wojciech Stawiarski <Wojciech.Stawiarski@nationalarchives.gov.uk>
2025-07-25 13:24:41 +0100

    DI extension for staging ingest

---
+++ Rdf/StagingIngestServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Api;
+using Rdf;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class StagingIngestServiceCollectionExtensions
+{
+    public static IServiceCollection AddStagingIngest(this IServiceCollection services)
+    {
+        services.AddSingleton<IStagingIngest<DriAccessCondition>, AccessConditionIngest>();
+        services.AddSingleton<IStagingIngest<DriLegislation>, LegislationIngest>();
+        services.AddSingleton<IStagingIngest<DriGroundForRetention>, GroundForRetentionIngest>();
+        services.AddSingleton<IStagingIngest<DriSubset>, SubsetIngest>();
+        services.AddSingleton<IStagingIngest<DriAsset>, AssetIngest>();
+        services.AddSingleton<IStagingIngest<DriVariation>, VariationIngest>();
+        services.AddSingleton<IStagingIngest<DriSensitivityReview>, SensitivityReviewIngest>();
+
+        return services;
+    }
+}
