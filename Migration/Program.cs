83746eb0c0081b1ead2a5aa79befff088a2460dd
Wojciech Stawiarski <Wojciech.Stawiarski@nationalarchives.gov.uk>
2025-07-29 10:04:30 +0100

    Logging configuration extracted to settings file

--- Migration/Program.cs
+++ Migration/Program.cs
@@ -2,19 +2,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Migration;
 
 var builder = Host.CreateApplicationBuilder();
-
-builder.Logging.AddFilter("System.Net.Http.HttpClient", LogLevel.Warning);
-
-builder.Logging.AddSimpleConsole(configure =>
-{
-    configure.SingleLine = true;
-    configure.IncludeScopes = false;
-    configure.TimestampFormat = "yyyy-MM-dd HH:mm:ss.fffffff ";
-});
 
 builder.Configuration.AddProgramCommandLine(args);
 
