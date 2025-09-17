5b2977ed6995f330bf69064b23574b6b930fb07f
Wojciech Stawiarski <Wojciech.Stawiarski@nationalarchives.gov.uk>
2025-09-17 08:48:35 +0100

    EndYear is not a number

--- Explorer/Models/Restriction.cs
+++ Explorer/Models/Restriction.cs
@@ -9,7 +9,7 @@ public class Restriction(INode node, IGraph graph) : DynamicNode(node, graph)
     public ICollection<DateTimeOffset?> ReviewDate => new DynamicObjectCollection<DateTimeOffset?>(this, Vocabulary.SensitivityReviewRestrictionReviewDate.Uri.ToString());
     public ICollection<DateTimeOffset?> CalculationStartDate => new DynamicObjectCollection<DateTimeOffset?>(this, Vocabulary.SensitivityReviewRestrictionCalculationStartDate.Uri.ToString());
     public ICollection<TimeSpan?> Duration => new DynamicObjectCollection<TimeSpan?>(this, Vocabulary.SensitivityReviewRestrictionDuration.Uri.ToString());
-    public ICollection<long?> EndYear => new DynamicObjectCollection<long?>(this, Vocabulary.SensitivityReviewRestrictionEndYear.Uri.ToString());
+    public ICollection<ILiteralNode> EndYear => new DynamicObjectCollection<ILiteralNode>(this, Vocabulary.SensitivityReviewRestrictionEndYear.Uri.ToString());
     public ICollection<string> Description => new DynamicObjectCollection<string>(this, Vocabulary.SensitivityReviewRestrictionDescription.Uri.ToString());
     public ICollection<UkLegislation> UkLegislations => new DynamicObjectCollection<UkLegislation>(this, Vocabulary.SensitivityReviewRestrictionHasLegislation.Uri.ToString());
     public ICollection<RetentionRestriction> RetentionRestriction => new DynamicObjectCollection<RetentionRestriction>(this, Vocabulary.SensitivityReviewRestrictionHasRetentionRestriction.Uri.ToString());
