58757808a95e23e7c22ff11949bed035d04d819d
Wojciech Stawiarski <Wojciech.Stawiarski@nationalarchives.gov.uk>
2025-09-19 15:13:11 +0100

    Partially revert premature commit

--- Api/IDriSqlExporter.cs
+++ Api/IDriSqlExporter.cs
@@ -7,5 +7,4 @@ public interface IDriSqlExporter
 {
     IEnumerable<DriAssetDeliverableUnit> GetAssetDeliverableUnits(int offset, CancellationToken cancellationToken);
     IEnumerable<DriVariationFile> GetVariationFiles(int offset, CancellationToken cancellationToken);
-    IEnumerable<DriChange> GetChanges(int offset, CancellationToken cancellationToken);
 }
