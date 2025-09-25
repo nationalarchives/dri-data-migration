ee2b6e83c0f9b5d7cef86a61dbcebec1b9e130d9
Wojciech Stawiarski <Wojciech.Stawiarski@nationalarchives.gov.uk>
2025-09-25 10:04:36 +0100

    Retrieve distinct changes

--- Dri/SqlExporter.cs
+++ Dri/SqlExporter.cs
@@ -71,7 +71,7 @@ public class SqlExporter(ILogger<SqlExporter> logger, IOptions<DriSettings> opti
         limit $limit offset $offset
         """;
     private readonly string auditSql = """
-        select t.TABLENAME, a.CHANGEREF, a.PRIMARYKEYVALUE, a.DATETIME, a.USERNAME, a.FULLNAME, a.XMLDIFF from auditchange a
+        select distinct t.TABLENAME, a.CHANGEREF, a.PRIMARYKEYVALUE, a.DATETIME, a.USERNAME, a.FULLNAME, a.XMLDIFF from auditchange a
         join tableinvolved t on t.TABLEINVOLVEDREF = a.TABLEINVOLVEDREF
         join dufile d on (d.DELIVERABLEUNITREF = a.PRIMARYKEYVALUE and t.TABLENAME = 'DeliverableUnit') or
         	(d.FILEREF = a.PRIMARYKEYVALUE and t.TABLENAME = 'DigitalFile')
