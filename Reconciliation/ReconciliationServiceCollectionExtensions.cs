363808a0217837c210acb1616c8779cc276bdbef
Wojciech Stawiarski <Wojciech.Stawiarski@nationalarchives.gov.uk>
2025-09-29 08:40:24 +0100

    Increase reconciliation SPARQL request timeout

--- Reconciliation/ReconciliationServiceCollectionExtensions.cs
+++ Reconciliation/ReconciliationServiceCollectionExtensions.cs
@@ -7,7 +7,7 @@ public static class ReconciliationServiceCollectionExtensions
 {
     public static IServiceCollection AddReconciliation(this IServiceCollection services)
     {
-        services.AddHttpClient<IReconciliationSparqlClient, ReconciliationSparqlClient>();
+        services.AddHttpClient<IReconciliationSparqlClient, ReconciliationSparqlClient>(h => h.Timeout = TimeSpan.FromMinutes(3));
         services.AddSingleton<IStagingReconciliationClient, StagingReconciliationClient>();
         services.AddHttpClient<IReconciliationSource, DiscoveryRecord>();
         services.AddSingleton<IReconciliationSource, PreservicaClosure>();
