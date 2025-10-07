6b97aea61f114da0cc55aae36c7deab37a30c11f
Wojciech Stawiarski <Wojciech.Stawiarski@nationalarchives.gov.uk>
2025-10-07 09:33:10 +0100

    Exclude changes made by a specific user

--- Dri/SqlExporter.cs
+++ Dri/SqlExporter.cs
@@ -80,7 +80,7 @@ public class SqlExporter(ILogger<SqlExporter> logger, IOptions<DriSettings> opti
         join tableinvolved t on t.TABLEINVOLVEDREF = a.TABLEINVOLVEDREF
         join dufile d on (d.DELIVERABLEUNITREF = a.PRIMARYKEYVALUE and t.TABLENAME = 'DeliverableUnit') or
         	(d.FILEREF = a.PRIMARYKEYVALUE and t.TABLENAME = 'DigitalFile')
-        where a.XMLDIFF is not null and d.Code = $code
+        where a.XMLDIFF is not null and instr(username, 'du') = 0 and d.Code = $code
         order by a.rowid
         limit $limit offset $offset
         """;
