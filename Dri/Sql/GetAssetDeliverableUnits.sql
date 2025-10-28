3bd1342107397bd78d3417ea1c79e964532b5abb
Wojciech Stawiarski <Wojciech.Stawiarski@nationalarchives.gov.uk>
2025-10-28 10:39:39 +0000

    Asset deliverable unit SQL query performance

--- Dri/Sql/GetAssetDeliverableUnits.sql
+++ Dri/Sql/GetAssetDeliverableUnits.sql
@@ -1,9 +1,17 @@
-﻿select d.DELIVERABLEUNITREF, d.CATALOGUEREFERENCE, x.XMLCLOB,
-    concat('[',group_concat(distinct json_object('id', f.FILEREF, 'location', f.FILELOCATION, 'name', f.NAME )),']') as files
-from dufile d
+﻿with dus as (
+	select d.DELIVERABLEUNITREF, d.CATALOGUEREFERENCE, d.DMETADATAREF from dufile d
+	where d.Code = $code
+    group by d.DELIVERABLEUNITREF, d.CATALOGUEREFERENCE, d.DMETADATAREF
+),
+files as (
+	select d.DELIVERABLEUNITREF, d.FILEREF, d.FILELOCATION, d.NAME from dufile d
+	where d.Code = $code
+)
+select d.DELIVERABLEUNITREF, d.CATALOGUEREFERENCE, x.XMLCLOB,
+	(select concat('[',
+		group_concat(distinct json_object('id', f.FILEREF, 'location', f.FILELOCATION, 'name', f.NAME )),']')
+	from files f where f.DELIVERABLEUNITREF = d.DELIVERABLEUNITREF) as files
+from dus d
 join xmlmetadata x on x.METADATAREF = d.DMETADATAREF
-join dufile f on f.DELIVERABLEUNITREF = d.DELIVERABLEUNITREF
-where d.Code = $code
-group by d.DELIVERABLEUNITREF, d.CATALOGUEREFERENCE, x.XMLCLOB
-order by d.rowid
+order by d.DELIVERABLEUNITREF
 limit $limit offset $offset
