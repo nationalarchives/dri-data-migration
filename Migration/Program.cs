3f8da69f129dc55249a41239a5f27b0996d7eebe
Wojciech Stawiarski <Wojciech.Stawiarski@nationalarchives.gov.uk>
2025-11-03 15:56:27 +0000

    Basic Open Telemetry configuration and infrustructure

--- Migration/Program.cs
+++ Migration/Program.cs
@@ -2,12 +2,31 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Migration;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Serilog;
 
 var builder = Host.CreateApplicationBuilder();
 
 builder.Configuration.AddProgramCommandLine(args);
+
+builder.Logging.AddOpenTelemetry(configure =>
+{
+    configure.IncludeScopes = true;
+    configure.IncludeFormattedMessage = true;
+});
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(configure => configure.AddService("DRI Migration"))
+    .WithMetrics(configure => configure.AddMeter("System.Net.Http")
+        .AddMeter("System.Net.NameResolution"))
+    .WithTracing(configure => configure.AddHttpClientInstrumentation())
+    .UseOtlpExporter(OtlpExportProtocol.Grpc, new Uri("http://localhost:4317/"));
 
 builder.Services.AddSerilog((_, loggerConfiguration) =>
     loggerConfiguration.ReadFrom.Configuration(builder.Configuration));
