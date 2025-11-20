9810fd9e13f283da1d9cc60c67cecaa8a01cb60c
Wojciech Stawiarski <Wojciech.Stawiarski@nationalarchives.gov.uk>
2025-11-20 10:47:36 +0000

    Blank node when it has type attribute and children element (WO 409 subset XML)

--- Staging/MalformedRdfRepair.cs
+++ Staging/MalformedRdfRepair.cs
@@ -43,13 +43,8 @@ internal static class MalformedRdfRepair
 
     internal static void Repair(XmlDocument doc, XmlElement node, XmlAttribute blankNode, bool isParentBlankNode)
     {
-        bool markedBlankNode = false;
-        var isBlankNode = node.ChildNodes.OfType<XmlElement>().Count() > 1 &&
-            (node.ParentNode is null || node.ParentNode.OfType<XmlElement>().Count() > 1);
-        var isPartOfBlankNode = node.ParentNode is not null && node.ParentNode.OfType<XmlElement>().Count() > 1 &&
-            node.ChildNodes.OfType<XmlElement>().Any() && node.ChildNodes.OfType<XmlElement>().All(e => e.FirstChild?.NodeType == XmlNodeType.Text);
-        var isEmbeddedBlankNode = isParentBlankNode && node.ChildNodes.OfType<XmlElement>().Any();
-        if (isBlankNode || isPartOfBlankNode || isEmbeddedBlankNode)
+        bool markedBlankNode = IsBlankNode(node, isParentBlankNode);
+        if (markedBlankNode)
         {
             markedBlankNode = true;
             node.Attributes.Append((XmlAttribute)blankNode.Clone());
@@ -61,15 +56,14 @@ internal static class MalformedRdfRepair
             {
                 if (attr.Name == "type")
                 {
-                    XmlAttribute typedAttr;
-                    if (markedBlankNode)
+                    var typedAttrName = "rdf:datatype";
+                    if (node.ChildNodes.OfType<XmlElement>().Any() && !markedBlankNode)
                     {
-                        typedAttr = doc.CreateAttribute("rdf:ID", NamespaceMapper.RDF);
+                        typedAttrName = "rdf:ID";
+                        markedBlankNode = true;
+                        allowedAttributes.Add((XmlAttribute)blankNode.Clone());
                     }
-                    else
-                    {
-                        typedAttr = doc.CreateAttribute("rdf:datatype", NamespaceMapper.RDF);
-                    }
+                    var typedAttr = doc.CreateAttribute(typedAttrName, NamespaceMapper.RDF);
                     typedAttr.Value = string.IsNullOrWhiteSpace(attr.Value) ?
                         "Undefined" : attr.Value.Trim().Replace(" & ", "_").Replace(" and ", "_")
                         .Replace("; ", "_").Replace(", ", "_").Replace(' ', '-')
@@ -94,5 +88,16 @@ internal static class MalformedRdfRepair
         {
             Repair(doc, childNode, blankNode, markedBlankNode);
         }
+    }
+
+    private static bool IsBlankNode(XmlElement node, bool isParentBlankNode)
+    {
+        var isBlankNode = node.ChildNodes.OfType<XmlElement>().Count() > 1 &&
+            (node.ParentNode is null || node.ParentNode.OfType<XmlElement>().Count() > 1);
+        var isPartOfBlankNode = node.ParentNode is not null && node.ParentNode.OfType<XmlElement>().Count() > 1 &&
+            node.ChildNodes.OfType<XmlElement>().Any() && node.ChildNodes.OfType<XmlElement>().All(e => e.FirstChild?.NodeType == XmlNodeType.Text);
+        var isEmbeddedBlankNode = isParentBlankNode && node.ChildNodes.OfType<XmlElement>().Any();
+
+        return isBlankNode || isPartOfBlankNode || isEmbeddedBlankNode;
     }
 }
