47f9fab0b31d1569ecd3d235428089ef0bf866f4
Wojciech Stawiarski <Wojciech.Stawiarski@nationalarchives.gov.uk>
2025-12-03 14:14:51 +0000

    WO 409 medical records separated material relationship

--- Exporter/RelationMapper.cs
+++ Exporter/RelationMapper.cs
@@ -10,7 +10,12 @@ internal static class RelationMapper
     {
         var related = graph.GetLiteralNodes(subject, Vocabulary.AssetRelationDescription).Select(l => l.Value)
             .Union(graph.GetLiteralNodes(subject, Vocabulary.AssetRelationIdentifier).Select(l => l.Value));
-        var separated = graph.GetLiteralNodes(subject, Vocabulary.AssetConnectedAssetNote).Select(l => l.Value);
+        var separated = (graph.GetLiteralNodes(subject, Vocabulary.AssetConnectedAssetNote).Select(l => l.Value)).ToList();
+        var wo409Separated = Wo409Separated(ReferenceBuilder.Build(null, assetReference));
+        if (wo409Separated is not null)
+        {
+            separated.Add(wo409Separated);
+        }
         var variations = graph.GetUriNodes(subject, Vocabulary.AssetHasVariation);
         List<string> variationRedactions = [];
         if (redactedVariationSequence is not null)
@@ -52,4 +57,32 @@ internal static class RelationMapper
     private static IEnumerable<RecordOutput.RecordRelationship> AssignRelationship(IEnumerable<string>? references,
         RecordOutput.RelationshipType relationshipType) => references is null ? [] :
         references.Select(r => new RecordOutput.RecordRelationship(relationshipType, r));
+
+    private static string? Wo409Separated(string assetReference) => assetReference switch
+    {
+        "WO/409/27/101/668" => "WO/409/27/101/1071",
+        "WO/409/27/102/20" => "WO/409/27/102/1059",
+        "WO/409/27/14/345" => "WO/409/27/14/537",
+        "WO/409/27/30/300" => "WO/409/27/30/1058",
+        "WO/409/27/4/46" => "WO/409/27/4/678",
+        "WO/409/27/51/301" => "WO/409/27/51/738",
+        "WO/409/27/70/26" => "WO/409/27/70/1074",
+        "WO/409/27/93/12" => "WO/409/27/93/662",
+        "WO/409/27/93/169" => "WO/409/27/93/663",
+        "WO/409/27/93/278" => "WO/409/27/93/664",
+        "WO/409/27/93/319" => "WO/409/27/93/665",
+
+        "WO/409/27/101/1071" => "WO/409/27/101/668",
+        "WO/409/27/102/1059" => "WO/409/27/102/20",
+        "WO/409/27/14/537" => "WO/409/27/14/345",
+        "WO/409/27/30/1058" => "WO/409/27/30/300",
+        "WO/409/27/4/678" => "WO/409/27/4/46",
+        "WO/409/27/51/738" => "WO/409/27/51/301",
+        "WO/409/27/70/1074" => "WO/409/27/70/26",
+        "WO/409/27/93/662" => "WO/409/27/93/12",
+        "WO/409/27/93/663" => "WO/409/27/93/169",
+        "WO/409/27/93/664" => "WO/409/27/93/278",
+        "WO/409/27/93/665" => "WO/409/27/93/319",
+        _ => null
+    };
 }
